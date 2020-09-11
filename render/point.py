830cd845a44bf1da226231bf939bdc1fbb80e8cb
guidiamond <guilherme.batista770@gmail.com>
2020-09-10 21:07:50 -0300

    render working

---
+++ render/point.py
@@ -0,0 +1,9 @@
+from .base.base import BaseRender
+
+class Point(BaseRender):
+    def __init__(self,points,colors):
+        super().__init__(points, colors)
+
+    def render(self):
+        for i in range(len(self.points)):
+            BaseRender.draw_pixel(self.points[i], self.colors[i])
